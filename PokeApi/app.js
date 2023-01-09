af1715a53782f4738da5bdab18dc68f380c56e62
julien93130fr <120179983+julien93130fr@users.noreply.github.com>
2023-01-09 12:12:48 +0100

    Add files via upload

---
+++ PokeApi/app.js
@@ -0,0 +1,29 @@
+//déclaration des varaibles avec l id du html
+
+let button = document.getElementById("button");
+let image = document.getElementById("image");
+let pokeNumber = document.getElementById("number");
+let pokeName = document.getElementById("name");
+
+const  changePokemon = async() => {
+    let randomNumber =Math.ceil(Math.random() *150 ) +1;
+
+    let requestString = `https://pokeapi.co/api/v2/pokemon/${randomNumber }  `;
+
+    // appelle de la requete avec fetch
+    let data  = await fetch(requestString);
+    console.log(data);
+    
+    let response = await data.json()
+    console.log(response);
+
+    // apelle de l 'image default
+    image.src =  response.sprites.front_default;
+
+    pokeNumber.textContent  = `${response.id}  `;
+
+    pokeName.textContent = response.name;
+
+}  
+changePokemon()
+button.addEventListener('click',changePokemon);
